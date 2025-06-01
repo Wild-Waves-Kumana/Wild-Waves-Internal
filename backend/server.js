6d06cb9e8ff8a0867a91247a60a4907692f822dd
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-06-01 19:04:11 +0530

    Add protected routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import protectedRoutes from './routes/protectedRoutes.js';
+
+
+
 
 dotenv.config();
 
@@ -18,6 +22,7 @@ connectDB();
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/protected', protectedRoutes); 
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
