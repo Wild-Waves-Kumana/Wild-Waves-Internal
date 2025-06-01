6b7403f40ed74ce641746292260f80f8ff350455
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-06-01 19:10:11 +0530

    Solve the problem of alart showing twice by using useRef

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import {jwtDecode} from 'jwt-decode';
 import { UserContext } from '../context/UserContext';
@@ -6,6 +6,7 @@ import { UserContext } from '../context/UserContext';
 const ProtectedRoute = ({ children }) => {
   const { isLoggedIn, logout } = useContext(UserContext);
   const location = useLocation();
+  const alertShownRef = useRef(false);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -18,7 +19,8 @@ const ProtectedRoute = ({ children }) => {
         const expiryTime = decoded.exp * 1000; // convert to milliseconds
         console.log('Token is valid until:', new Date(expiryTime).toLocaleTimeString());
 
-        if (decoded.exp < now) {
+        if (decoded.exp < now && !alertShownRef.current) {
+          alertShownRef.current = true; // ✅ only allow one alert
           alert('Session expired. Please log in again.');
           logout();
         }
