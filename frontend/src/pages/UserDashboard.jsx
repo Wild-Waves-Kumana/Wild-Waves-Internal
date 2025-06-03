b73aa838516d8f2c8aa392dfabe4262ec15b3b25
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-06-03 14:22:24 +0530

    create a dummy userdashboard for navigate login as a user

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
 
 const UserDashboard = () => {
+  const navigate = useNavigate();
+  const { username, logout } = useContext(UserContext);
+
+  const handleLogout = () => {
+    logout();            // clear login state
+    navigate('/');       // go back to login
+  };
   return (
-     <div className="flex items-center justify-center min-h-screen bg-green-100">
-      <h1 className="text-4xl font-bold text-green-800">Welcome to Dashboard 🎉</h1>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4">
+      <h1 className="text-4xl font-bold text-green-800">Welcome to User Dashboard 🎉</h1>
+      <h1 className="text-3xl pb-4 font-bold">Welcome, {username} 👋</h1>
+    <div>
+      <button
+        onClick={handleLogout}
+        className="mt-8 bg-red-500 text-white px-6 py-2 rounded hover:bg-red-600">
+        Logout
+      </button>
+    </div>
+      
+
     </div>
   )
 }
