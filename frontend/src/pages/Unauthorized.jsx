d97e913fda1e09c89b875e934707f48a51136e87
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-06-03 14:23:28 +0530

    Created a unauthorized.jsx for catch the unauthorized logins and navigates

---
+++ frontend/src/pages/Unauthorized.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Unauthorized = () => (
+  <div className="text-center mt-10 text-xl text-red-600">
+    ❌ You do not have permission to access this page.
+  </div>
+);
+
+
+export default Unauthorized
