7cc1b4d3b3367e7c41aa2811c7b0522602f1d6cb
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-06-26 17:20:31 +0530

    Updated the user modelr for user registration with Room name, room id

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+  roomname: { type: String, required: true },
+  roomid: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true,  default: 'user' },
