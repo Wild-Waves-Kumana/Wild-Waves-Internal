81a5359800d60d2c93aeb98aeff4bc080b5ae10a
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-06-26 17:21:00 +0530

    Added equipments models

---
+++ backend/models/light.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const lightSchema = new mongoose.Schema({
+  itemName: String,
+  itemCode: { type: String, unique: true },
+  username: String,
+  status: { type: String, enum: ['ON', 'OFF'], default: 'ON' }
+});
+
+export default mongoose.model('Light', lightSchema);
