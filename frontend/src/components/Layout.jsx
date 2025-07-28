d286c79f0c9da9c9557377f3519c0a78f21cebf8
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-07-29 02:19:22 +0530

    Developed the navbar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen">
+      {/* Sidebar on the left */}
       <Sidebar />
-      <main className="flex-1 p-6 bg-gray-100 min-h-screen">{children}</main>
+      {/* Main content area */}
+      <div className="flex flex-col flex-1">
+        {/* Navbar at the top of main area */}
+        <Navbar />
+        <main className="flex-1 p-6 bg-gray-100">{children}</main>
+      </div>
     </div>
   );
 };
