266901751416a315f7f2e6db23f096c8ddd9e16e
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-07-29 10:12:40 +0530

    Add UserLightList component to EquipmentUser page

--- frontend/src/pages/EquipmentUser.jsx
+++ frontend/src/pages/EquipmentUser.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import UserDoorList from '../components/UserDoorList'
+import UserLightList from '../components/UserLightList'
 
 const EquipmentUser = () => {
   return (
     <div>
       <UserDoorList/>
+
+      <UserLightList/>
     </div>
   )
 }
