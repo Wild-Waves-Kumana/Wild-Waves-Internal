988fa80045a92f2ad497ef0d467b32ade0142a36
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-08-05 17:27:13 +0530

    Add adminId field to user schema for admin association

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,6 +8,9 @@ const userSchema = new mongoose.Schema({
   role: { type: String, required: true,  default: 'user' },
   adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' }, // <-- added
   companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+  doors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Door' }],
+  lights: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Light' }],
+  airConditioners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'AirConditioner' }],
 });
 
 export default mongoose.model('User', userSchema);
