a38b6ea840e9179269c89f63f5c19aaca070ee72
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-08-29 14:56:27 +0530

    Add setCartStatus route to foodCartRoutes for updating cart status

--- backend/routes/foodCartRoutes.js
+++ backend/routes/foodCartRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { addToCart, getCartItems, editCartItems } from "../controllers/foodCartController.js";
+import { addToCart, getCartItems, editCartItems, setCartStatus } from "../controllers/foodCartController.js";
 
 const router = express.Router();
 
 router.post("/add", addToCart);
 router.get("/items/:userId", getCartItems);
 router.put("/edit-cart", editCartItems);
+router.put("/set-status/:cartId", setCartStatus);
+
 
 export default router;
