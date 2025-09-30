8c901df2e9a79a91897bae91b851a5ce3329f583
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-10-01 01:14:04 +0530

    fix: Change background color of UserDashboard from green to gray

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DashboardController from '../components/DashboardController';
 
 const UserDashboard = () => (
-  <div className="min-h-screen bg-green-500 flex flex-col items-center px-4">
+  <div className="min-h-screen bg-gray-100 flex flex-col items-center px-4">
     <DashboardController />
   </div>
 );
