97be7793dd5c93ca1047ba3732dcee5c19328c65
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-10-01 16:30:47 +0530

    fix: Implement sidebar toggle functionality in Layout component

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="flex min-h-screen">
-      {/* Sidebar on the left */}
-      <Sidebar />
+      {/* Sidebar: always overlays, auto-hide on close */}
+      <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
       {/* Main content area */}
       <div className="flex flex-col flex-1">
-        {/* Navbar at the top of main area */}
-        <Navbar />
+        {/* Navbar with hamburger */}
+        <Navbar setSidebarOpen={setSidebarOpen} />
         <main className="flex-1 p-6 bg-gray-100">{children}</main>
       </div>
     </div>
