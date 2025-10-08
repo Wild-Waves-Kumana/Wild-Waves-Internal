c224dccbe796a63af172e27e4d8bacba0bfcd7d6
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-10-08 13:58:23 +0530

    feat: Refactor CompanyList component and integrate it into CompanyDashboard

---
+++ frontend/src/pages/CompanyDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CompanyList from '../components/lists/CompanyList';
+
+const CompanyDashboard = () => {
+  return (
+    <div >
+      <CompanyList />
+    </div>
+  );
+};
+
+export default CompanyDashboard;
