58db8bbede4fd8426b94eada6ba36dceae8da3ae
Chathura Dissanayake <chathurakosala6@gmail.com>
2025-11-01 11:27:56 +0530

    feat: Add name field to booking schema for enhanced user information

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
   bookingId: { type: String, required: true, unique: true },
+  name: { type: String },
   email: { type: String, required: true },
   contactNumber: { type: String, required: true },
-
+  selectedDates: [{ type: Date }], // Array to store multiple dates
 });
+
 export default mongoose.model("Booking", bookingSchema);
